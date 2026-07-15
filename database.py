c399d00ac0b36ad74100d0f434620cc55a3389ac
TOM-BOT-12 <tt0908538612@gmail.com>
2026-07-15 18:00:19 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,20 @@
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+CHANNELS = [
+    "@binance_red_packet12",
+    "@hf68ng9hc",
+    "@Buzzerfollowbake",
+    "@lefegegc"
+]
+
+REFERRAL_REWARD = 3
+TIKTOK_REWARD = 3
+
+MIN_WITHDRAW = 100
