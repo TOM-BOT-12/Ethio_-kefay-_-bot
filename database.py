44fe878176df7964c36f610c338b605194a116bf
TOM-BOT-12 <tt0908538612@gmail.com>
2026-07-15 18:07:33 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,34 @@
 
-import os
-from dotenv import load_dotenv
+import aiosqlite
 
-load_dotenv()
+DB_NAME = "bot.db"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+async def create_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT,
+            points INTEGER DEFAULT 0,
+            referrals INTEGER DEFAULT 0
+        )
+        """)
+        await db.commit()
 
-CHANNELS = [
-    "@binance_red_packet12",
-    "@hf68ng9hc",
-    "@Buzzerfollowbake",
-    "@lefegegc"
-]
 
-REFERRAL_REWARD = 3
-TIKTOK_REWARD = 3
+async def add_user(user_id, username):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            "INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)",
+            (user_id, username)
+        )
+        await db.commit()
 
-MIN_WITHDRAW = 100
+
+async def get_user(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        cursor = await db.execute(
+            "SELECT * FROM users WHERE user_id=?",
+            (user_id,)
+        )
+        return await cursor.fetchone()
