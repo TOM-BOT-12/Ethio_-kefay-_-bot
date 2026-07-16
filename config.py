d94ada8ce813a3a217fd4a66244081922c24a1d4
TOM-BOT-12 <tt0908538612@gmail.com>
2026-07-16 19:49:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-
 import os
 from dotenv import load_dotenv
 
+# .env ፋይልን ለማንበብ
 load_dotenv()
 
+# ዋና ሚስጥራዊ ቁልፎች (በኋላ በRender ላይ Environment Variable አድርገን የምናስገባቸው)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7800213349"))
 
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-# Force Join Channels
+# ተጠቃሚው ግዴታ Join ማድረግ ያለባቸው ቻናሎች ዝርዝር
 CHANNELS = [
     "@binance_red_packet12",
     "@hf68ng9hc",
@@ -16,9 +16,9 @@ CHANNELS = [
     "@lefegegc"
 ]
 
-# Reward Settings
-REFERRAL_REWARD = 3  # ETB
-TIKTOK_REWARD = 3   # ETB
+# የሽልማት መጠኖች (በብር/ETB)
+REFERRAL_REWARD = 3.0  # ለአንድ ሪፈራል የሚከፈል
+TIKTOK_REWARD = 3.0    # ለቲክቶክ ተግባር የሚከፈል
 
-# Withdraw Settings
-MIN_WITHDRAW = 100  # ETB
+# ዝቅተኛው የማውጫ መጠን (በብር/ETB)
+MIN_WITHDRAW = 100.0
